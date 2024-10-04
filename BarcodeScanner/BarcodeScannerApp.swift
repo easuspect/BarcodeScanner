10cad8086c3d57c153074044886502963aa65668
Tolga <tolgatelseren@gmail.com>
2024-10-04 08:34:04 -0700

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Tolga Telseren on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
---
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  BarcodeScanner
+//
+//  Created by Tolga Telseren on 10/2/24.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+                
+                NavigationView {
+                    VStack {
+                        Rectangle()
+                            .frame(width: .infinity, height: 300)
+                        
+                        Spacer().frame(height: 60)
+                        
+                        Label("Scanned Barcode", systemImage: "barcode.viewfinder")
+                            .font(.title)
+                        
+                        Text("Not Yet Scanned")
+                            .bold()
+                            .font(.largeTitle)
+                            .foregroundColor(.green)
+                            .padding()
+                    
+                    }
+                    .navigationTitle("Barcode Scanner")
+                }
+            }
+        }
+
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                BarcodeScannerView()
+            }
+        }
+ 
+
